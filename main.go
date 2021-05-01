57fd5fc511360fdcc7585fe26316e6615cb8fda9
tvblackman1 <vbif.misha2012@yandex.ru>
2021-05-02 01:43:05 +0300

    try add mysql db

--- main.go
+++ main.go
@@ -1,15 +1,23 @@
 package main
 
 import (
+	"database/sql"
 	"encoding/json"
 	"fmt"
+	_ "github.com/go-sql-driver/mysql"
 	"github.com/gorilla/mux"
 	"log"
-	//"math/rand"
 	"net/http"
 	"os"
-	//"strconv"
 )
+
+type User struct {
+	ID       int8   `json:"id"`
+	Login    string `json:"login"`
+	Password string `json:"password"`
+}
+
+
 
 type Device struct {
 	ID          string      `json:"id"`
@@ -77,6 +85,19 @@ func forGuest(w http.ResponseWriter, r *http.Request) {
 }
 
 func main() {
+
+	fmt.Println("Go MySQL Tutorial")
+	db, err := sql.Open("mysql", "username:password@tcp(127.0.0.1:3306)/test")
+	if err != nil {
+		panic(err.Error())
+	}
+
+	// defer the close till after the main function has finished
+	// executing
+	defer db.Close()
+
+	fmt.Println("!!")
+
 	r := mux.NewRouter()
 	devices = append(devices, Device{
 		ID: "weather-station",
